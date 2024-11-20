853e99e21ac083dd922ad55a86ed3f9223805449
fernandogerson <fluqueg@unsa.edu.pe>
2024-11-19 22:13:01 -0500

    Abriendo el archivo csv y leyendolo

--- cgi-bin/script.pl
+++ cgi-bin/script.pl
@@ -6,3 +6,8 @@ use Text::CSV;
 
 my $cgi = CGI->new;
 print $cgi->header("text/html; charset=UTF-8");
+
+my $csvArchivo = "/usr/lib/cgi-bin/Data_Universidades_LAB06.csv";
+my @universidades;
+my @resultados;
+my $csv = Text::CSV->new({ binary => 1, auto_diag => 1 });
