114828afacdcfcc3000370814bce0ecf131a9ce2
fernandogerson <fluqueg@unsa.edu.pe>
2024-11-19 22:47:14 -0500

    Iterando en el arreglo donde almacenamos las referencias de cada columna y mostrarlas cuando existan coincidencias con las busquedas

--- cgi-bin/script.pl
+++ cgi-bin/script.pl
@@ -49,3 +49,24 @@ print <<'HTML';
     <h1>Resultados de la búsqueda</h1>
 HTML
 
+if (@resultados) {
+    print "<table border='1'>";
+    print "<tr><th>Código</th><th>Nombre</th><th>Tipo</th><th>Licenciamiento</th><th>Fecha Inicio</th><th>Fecha Fin</th><th>Periodo</th><th>Departamento</th><th>Ubicación</th></tr>";
+    foreach my $universidad (@resultados) {
+        my ($codigo, $nombre, $tipo, $licenciamiento, $fecha_inicio, $fecha_fin, $periodo, $departamento, $latitud, $longitud) = @$universidad;
+
+        my $google_maps_link = qq{<a href="https://www.google.com/maps/search/?api=1&query=$latitud,$longitud" target="_blank">Ver en Google Maps</a>};
+        print "<tr><td>$codigo</td><td>$nombre</td><td>$tipo</td><td>$licenciamiento</td><td>$fecha_inicio</td><td>$fecha_fin</td><td>$periodo</td><td>$departamento</td><td>$google_maps_link</td></tr>";
+    }
+    print "</table>";
+} else {
+    print "<p>No se encontraron coincidencias.</p>";
+}
+
+print <<'HTML';
+</body>
+</html>
+HTML
+
+
+
